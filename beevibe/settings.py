47220713c2ecc43711bbcb1699d75aa71baef6d1
snickalls <stephennickalls@gmail.com>
2023-10-17 15:23:23 +1300

    Test secret is ignored

--- beevibe/settings.py
+++ beevibe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .secret import Secret
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,7 +93,7 @@ DATABASES = {
         'NAME': 'beevibe',
         'HOST': 'localhost',
         'USER': 'root',
-        'PASSWORD': ''
+        'PASSWORD': Secret.secret(),
     }
 }
 
